0c3e93316820b26435813c5432443f807eba0b3a
NajibOsman <machidydemajka38@gmail.com>
2017-02-14 16:48:48 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -23,3 +23,10 @@ var add = function(number1, number2) {
                   var number2 =parseInt (prompt("Enter another number:"));
 
                   alert(multiply(number1,number2));
+                  var divide = function(number1, number2) {
+                        return number1 / number2;
+                      };
+                  var number1 =parseInt (prompt("Enter a number:"));
+                  var number2 =parseInt (prompt("Enter another number:"));
+
+                  alert(divide(number1,number2));
